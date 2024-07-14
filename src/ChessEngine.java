80390eb3b0a9a32764e86ef8a620f3a44aa4b510
SirPatschiii <tofue2003@gmx.de>
2024-07-14 18:36:09 +0200

    Implemented version 0.2 of the application. Yet there is no bot implemented. The foundation is build and the player can make legal moves and undo moves. The validation of moves is implemented in its first stage.

--- src/ChessEngine.java
+++ src/ChessEngine.java
@@ -1,50 +1,64 @@
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Stack;
 
 /**
- * The {@code ChessEngine} class represents the core logic of a chess engine.
- *
+ * <p>
+ * The {@code ChessEngine} class represents the core logic of the chess engine.
+ * </p>
  * <p>
  * It manages the game state, validates moves, handles move history, and interacts with the graphical user interface (GUI).
- * </p>
- *
- * <p>
  * The game state is stored using a {@link HashMap} where each {@link EPieceAbbreviation} is mapped to its corresponding bitboard.
- * </p>
- *
- * <p>
  * This class initializes the game with default positions and provides methods to check move validity, execute moves,
  * and undo the last move.
  * </p>
- *
  * <p>
  * Note: This class assumes a 64-square chessboard represented by bitboards for efficient move generation and validation.
  * </p>
  *
  * @author SirPatschiii
- * @version 2024-06-26
+ * @version 14.07.2024
  */
+@SuppressWarnings("unused")
 public class ChessEngine {
     private final GUI cGUI;
-    private final MouseKeyboard cMouseKeyboard;
 
     private final HashMap<EPieceAbbreviation, Long> gameState;
+    private boolean whiteToMove;
     private final Stack<Move> moveHistory;
 
     // Constant bitboards for default position
-    private static final long DEFAULT_BB_WP = 0x000000000000FF00L;
-    private static final long DEFAULT_BB_WN = 0x0000000000000042L;
-    private static final long DEFAULT_BB_WB = 0x0000000000000024L;
-    private static final long DEFAULT_BB_WR = 0x0000000000000081L;
-    private static final long DEFAULT_BB_WQ = 0x0000000000000010L;
-    private static final long DEFAULT_BB_WK = 0x0000000000000008L;
-    private static final long DEFAULT_BB_BP = 0x00FF000000000000L;
-    private static final long DEFAULT_BB_BN = 0x4200000000000000L;
-    private static final long DEFAULT_BB_BB = 0x2400000000000000L;
-    private static final long DEFAULT_BB_BR = 0x8100000000000000L;
-    private static final long DEFAULT_BB_BQ = 0x1000000000000000L;
-    private static final long DEFAULT_BB_BK = 0x0800000000000000L;
+    public static final long DEFAULT_BB_WP = 0x000000000000FF00L;
+    public static final long DEFAULT_BB_WN = 0x0000000000000042L;
+    public static final long DEFAULT_BB_WB = 0x0000000000000024L;
+    public static final long DEFAULT_BB_WR = 0x0000000000000081L;
+    public static final long DEFAULT_BB_WQ = 0x0000000000000010L;
+    public static final long DEFAULT_BB_WK = 0x0000000000000008L;
+    public static final long DEFAULT_BB_BP = 0x00FF000000000000L;
+    public static final long DEFAULT_BB_BN = 0x4200000000000000L;
+    public static final long DEFAULT_BB_BB = 0x2400000000000000L;
+    public static final long DEFAULT_BB_BR = 0x8100000000000000L;
+    public static final long DEFAULT_BB_BQ = 0x1000000000000000L;
+    public static final long DEFAULT_BB_BK = 0x0800000000000000L;
+
+    // Constant bitboards for ranks and files
+    public static final long RANK_1 = 0x00000000000000FFL;
+    public static final long RANK_2 = 0x000000000000FF00L;
+    public static final long RANK_3 = 0x0000000000FF0000L;
+    public static final long RANK_4 = 0x00000000FF000000L;
+    public static final long RANK_5 = 0x000000FF00000000L;
+    public static final long RANK_6 = 0x0000FF0000000000L;
+    public static final long RANK_7 = 0x00FF000000000000L;
+    public static final long RANK_8 = 0xFF00000000000000L;
+    public static final long FILE_A = 0x8080808080808080L;
+    public static final long FILE_B = 0x4040404040404040L;
+    public static final long FILE_C = 0x2020202020202020L;
+    public static final long FILE_D = 0x1010101010101010L;
+    public static final long FILE_E = 0x0808080808080808L;
+    public static final long FILE_F = 0x0404040404040404L;
+    public static final long FILE_G = 0x0202020202020202L;
+    public static final long FILE_H = 0x0101010101010101L;
 
     /**
      * Constructs a new {@code ChessEngine} instance.
@@ -55,10 +69,11 @@ public class ChessEngine {
      */
     public ChessEngine() {
         gameState = new HashMap<>();
+        whiteToMove = true;
         moveHistory = new Stack<>();
         initializeGameState();
 
-        cMouseKeyboard = new MouseKeyboard(this);
+        MouseKeyboard cMouseKeyboard = new MouseKeyboard(this);
         cGUI = new GUI(this, cMouseKeyboard);
     }
 
@@ -100,17 +115,14 @@ public class ChessEngine {
     public void checkMove(byte squareFrom, byte squareTo) {
         // Create new move with entered data from the user and the game state bevor the move is executed
         @SuppressWarnings("unchecked")
-        Move move = new Move(squareFrom, squareTo, (HashMap<EPieceAbbreviation, Long>) gameState.clone());
+        Move movePlayed = new Move(squareFrom, squareTo, (HashMap<EPieceAbbreviation, Long>) gameState.clone());
 
-        // Checks if move is valid
-        if (isMoveValid()) {
-            // Adds move to move history
-            moveHistory.push(move);
-            System.out.println(move);
-            // Recalculates index to bitboard index
-            squareFrom = (byte) (63 - squareFrom);
-            squareTo = (byte) (63 - squareTo);
-            for (Map.Entry<EPieceAbbreviation, Long> entry : getGameState().entrySet()) {
+        // Checks if the played move is valid
+        if (isMoveValid(movePlayed)) {
+            // Adds the played move to move history
+            moveHistory.push(movePlayed);
+
+            for (Map.Entry<EPieceAbbreviation, Long> entry : gameState.entrySet()) {
                 // Deletes piece on target square if existent
                 if (BitHelper.isBitSet(entry.getValue(), squareTo)) {
                     entry.setValue(BitHelper.clearBit(entry.getValue(), squareTo));
@@ -121,30 +133,95 @@ public class ChessEngine {
                     entry.setValue(BitHelper.setBit(entry.getValue(), squareTo));
                 }
             }
+
+            // Transfers the right to move
+            whiteToMove = !whiteToMove;
+
             // Refreshes graphics
             cGUI.render();
         }
     }
 
     /**
-     * Checks if the current move is valid.
-     *
      * <p>
-     * Placeholder method for move validation logic.
+     * Generates a list of all possible moves for the player whose turn it is,
+     * based on the current game state. The method checks if it's white's turn or black's turn
+     * and generates moves accordingly for each piece type: Pawn, Knight, Bishop, Rook, Queen, and King.
      * </p>
      *
-     * @return {@code true} if the move is valid; {@code false} otherwise.
+     * @return An {@code ArrayList} of {@link Move} objects representing all possible moves for the current player.
      */
-    public boolean isMoveValid() {
-        // Placeholder implementation
-        return true;
+    public ArrayList<Move> generateAllPossibleMoves() {
+        ArrayList<Move> possibleMoves = new ArrayList<>();
+
+        // Generate moves for white pieces
+        if (whiteToMove) {
+            for (Move move : Pawn.generatePossibleWhitePawnMoves(gameState)) {
+                possibleMoves.addLast(move);
+            }
+            for (Move move : Knight.generatePossibleWhiteKnightMoves(gameState)) {
+                possibleMoves.addLast(move);
+            }
+            for (Move move : Bishop.generatePossibleWhiteBishopMoves(gameState)) {
+                possibleMoves.addLast(move);
+            }
+            for (Move move : Rook.generatePossibleWhiteRookMoves(gameState)) {
+                possibleMoves.addLast(move);
+            }
+            for (Move move : Queen.generatePossibleWhiteQueenMoves(gameState)) {
+                possibleMoves.addLast(move);
+            }
+            for (Move move : King.generatePossibleWhiteKingMoves(gameState)) {
+                possibleMoves.addLast(move);
+            }
+        }
+
+        // Generate moves for black pieces
+        if (!whiteToMove) {
+            for (Move move : Pawn.generatePossibleBlackPawnMoves(gameState)) {
+                possibleMoves.addLast(move);
+            }
+            for (Move move : Knight.generatePossibleBlackKnightMoves(gameState)) {
+                possibleMoves.addLast(move);
+            }
+            for (Move move : Bishop.generatePossibleBlackBishopMoves(gameState)) {
+                possibleMoves.addLast(move);
+            }
+            for (Move move : Rook.generatePossibleBlackRookMoves(gameState)) {
+                possibleMoves.addLast(move);
+            }
+            for (Move move : Queen.generatePossibleBlackQueenMoves(gameState)) {
+                possibleMoves.addLast(move);
+            }
+            for (Move move : King.generatePossibleBlackKingMoves(gameState)) {
+                possibleMoves.addLast(move);
+            }
+        }
+
+        return possibleMoves;
+    }
+
+    public boolean isMoveValid(Move movePlayed) {
+        ArrayList<Move> possibleMoves = generateAllPossibleMoves();
+
+        // Iterate through all possible moves
+        for (Move possibleMove : possibleMoves) {
+            // Check if the played move is listed in the possible moves
+            if (movePlayed.hashCode() == possibleMove.hashCode()) {
+                return true;
+            }
+        }
+        // If not, return false
+        return false;
     }
 
     /**
-     * Undoes the last move.
-     *
      * <p>
-     * Restores the game state to the state before the last move, updates the GUI, and removes the move from history.
+     * Undoes the last move.
+     * </p>
+     * <p>
+     * Restores the game state to the state before the last move, updates the GUI, removes the move from history
+     * and transfers the right to move.
      * </p>
      */
     public void undoMove() {
@@ -159,69 +236,20 @@ public class ChessEngine {
                 gameState.replace(entry.getKey(), entry.getValue());
             }
 
+            // Transfers the right to move
+            whiteToMove = !whiteToMove;
+
             // Refreshes graphics
             cGUI.render();
         }
     }
 
-    // Getter methods for default bitboards
-    // (These methods are static because they access constants)
-    public static long getDefaultBbWp() {
-        return DEFAULT_BB_WP;
-    }
-
-    public static long getDefaultBbWn() {
-        return DEFAULT_BB_WN;
-    }
-
-    public static long getDefaultBbWb() {
-        return DEFAULT_BB_WB;
-    }
-
-    public static long getDefaultBbWr() {
-        return DEFAULT_BB_WR;
-    }
-
-    public static long getDefaultBbWq() {
-        return DEFAULT_BB_WQ;
-    }
-
-    public static long getDefaultBbWk() {
-        return DEFAULT_BB_WK;
-    }
-
-    public static long getDefaultBbBp() {
-        return DEFAULT_BB_BP;
-    }
-
-    public static long getDefaultBbBn() {
-        return DEFAULT_BB_BN;
-    }
-
-    public static long getDefaultBbBb() {
-        return DEFAULT_BB_BB;
-    }
-
-    public static long getDefaultBbBr() {
-        return DEFAULT_BB_BR;
-    }
-
-    public static long getDefaultBbBq() {
-        return DEFAULT_BB_BQ;
-    }
-
-    public static long getDefaultBbBk() {
-        return DEFAULT_BB_BK;
-    }
-
     /**
-     * Retrieves the current game state.
-     *
      * <p>
-     * Returns the {@link HashMap} representing the current game state where each piece is mapped to its bitboard.
+     * Retrieves the current game state.
      * </p>
      *
-     * @return The current game state as a {@link HashMap} of {@link EPieceAbbreviation} to {@code long} values.
+     * @return The current game state as a {@code HashMap} of {@link EPieceAbbreviation} to {@code long} values.
      */
     public HashMap<EPieceAbbreviation, Long> getGameState() {
         return gameState;
